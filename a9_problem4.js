39478f4f28899b7bd082578f27fd5dd14161066c
Asim M <asim@Winterfell.local>
2016-10-31 22:36:28 -0400

    intializing the project

---
+++ a9_problem4.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const dns = require('dns');
+const os = require('os');
+
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  var quote = '<b><i>Four things support the world:</i></br> the learning of the wise, </br>the justice of the great, </br>the prayers of the good, </br>and the valor of the brave';
+  res.end(quote);
+
+});
+
+server.listen(port , () => {
+  console.log(`Server is open for business...`);
+});
