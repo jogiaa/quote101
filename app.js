74be07cd8df6419f828fab0f817faa671a1f6f16
Asim M <asim@Winterfell.local>
2016-10-31 23:34:08 -0400

    making changes

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 const http = require('http');
-const dns = require('dns');
-const os = require('os');
 
-
-const port = 80;
+const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
